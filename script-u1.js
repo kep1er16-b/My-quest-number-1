51590696c3b0cdc2fa48ebe4eaf0a762e0131aee
Viktor-Dev <smile.to.alice@gmail.com>
2022-05-09 20:10:17 +0300

    Quest number 1

---
+++ script-u1.js
@@ -0,0 +1,31 @@
+// #1
+
+// let money = prompt('Ваш бюджет на месяц?');
+// let time = prompt('Введите дату в формате YYYY-MM-DD', "YYYY-MM-DD");
+
+// #4
+
+// let expenses = {
+//     expensesMoontly: prompt('Введите обязательную статью расходов в этом месяце'),
+//     expensesHowMuch: prompt("Во сколько обойдется?")
+// };
+
+// console.log(expenses.expensesMoontly, expenses.expensesHowMuch);
+
+// #3
+
+var appData = {
+    money: +prompt("Ваш бюджет на месяц?", "0"),
+    time: prompt('Введите дату в формате YYYY-MM-DD', "YYYY-MM-DD"),
+    expenses: {
+            expensesMoontly: prompt('Введите обязательную статью расходов в этом месяце'),
+            expensesHowMuch: prompt("Во сколько обойдется?"),
+            expensesMoontly1: prompt('Введите обязательную статью расходов в этом месяце'),
+            expensesHowMuch1: prompt("Во сколько обойдется?")
+        },
+    optionalExpenses: "",
+    income: [''],
+    savings: 0     
+    };
+    
+    alert(appData.money/30);
